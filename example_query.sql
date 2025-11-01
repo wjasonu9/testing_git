d5c5c4a9b82a0aa2e2efa4743124175eed4595b0
wjasonu9 <48846408+wjasonu9@users.noreply.github.com>
2025-11-01 13:42:05 -0700

    Create sql file with example query

---
+++ example_query.sql
@@ -0,0 +1,9 @@
+CREATE TABLE qjx AS
+SELECT job_postings_fact.*, company_dim.name, company_dim.link, company_dim.link_google, company_dim.thumbnail
+FROM job_postings_fact
+INNER JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id;
+--LIMIT 36;
+SELECT name AS company, COUNT(job_id) AS n_job
+FROM qjx
+GROUP BY name
+ORDER BY n_job DESC;
