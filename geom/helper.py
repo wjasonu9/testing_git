c928ae11ff84516f9551eda3b698fec0e8d3a143
wjasonu9 <wjasonu9@gmail.com>
2026-06-09 18:09:07 -0400

    start ch2

--- geom/helper.py
+++ geom/helper.py
@@ -24,3 +24,22 @@ def show_geometry_image(img):
     plt.axis("off")
     plt.show()
     plt.close()
+def get_intersection(p1, p2, p3, p4, Int=False):
+    """Finds the intersection point of two lines defined by (p1, p2) and (p3, p4).
+    Assumes lines are not parallel; no error handling for parallel/collinear lines."""
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    x4, y4 = p4
+    denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+    intersect_x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom
+    intersect_y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom
+    if not Int:
+        return (intersect_x, intersect_y)
+    return (int(intersect_x), int(intersect_y))
+def draw_perp(img, pt, line_p1, line_p2, color=(0, 0, 0), thickness=1):
+    """Draws a perpendicular segment from pt to the line defined by (line_p1, line_p2)"""
+    dx, dy = line_p2[0] - line_p1[0], line_p2[1] - line_p1[1] #get line's direction vector
+    perp_pt2 = (pt[0] - dy, pt[1] + dx) #another point on perpendicular line
+    proj_pt = get_intersection(line_p1, line_p2, pt, perp_pt2,Int=True)
+    cv2.line(img, pt, proj_pt, color, thickness)
