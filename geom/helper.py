14c4cbd03027e669582470c1dd00e3b07ef12150
wjasonu9 <wjasonu9@gmail.com>
2026-06-29 19:48:05 -0400

    finish ch3

--- geom/helper.py
+++ geom/helper.py
@@ -13,11 +13,13 @@ def double_arrow_line(img, pt1, pt2, color = (0, 0, 0), thickness=2, tip_length=
     cv2.arrowedLine(img, m, pt1, color, thickness, tipLength=tip_length * 2)
     cv2.arrowedLine(img, m, pt2, color, thickness, tipLength=tip_length * 2)
 def add_tick(img, pt1, pt2, offset_px=0):
-    mx, my = (pt1[0] + pt2[0]) // 2 + offset_px, (pt1[1] + pt2[1]) // 2
+    mx, my = midpt(pt1,pt2)
     dx, dy = pt2[0] - pt1[0], pt2[1] - pt1[1]
     length = np.hypot(dx, dy)
-    nx, ny = int(-dy / length * 8), int(dx / length * 8)
-    cv2.line(img, (mx - nx, my - ny), (mx + nx, my + ny), color, thickness=2)
+    dx, dy = dx/length, dy/length
+    mx, my = mx+int(dx*offset_px), my+int(dy*offset_px)
+    nx, ny = int(-dy * 8), int(dx * 8)
+    cv2.line(img, (mx - nx, my - ny), (mx + nx, my + ny), color, thickness=1)
 def show_geometry_image(img,x_px=200,y_px=200):
     # x_px, y_px: how many pixels correspond to 1 inch
     h_px, w_px, _ = img.shape
@@ -71,3 +73,14 @@ def draw_path(img, *pts, color=(0, 0, 0), thickness=2):
         return None
     for j in range(len(pts) - 1):
         cv2.line(img, pts[j], pts[j + 1], color, thickness)
+def find_eq_tri(pt1, pt2, orient=1):
+    """Finds pt3 such that pt1, pt2, and pt3 form an equilateral triangle.
+    When orient=1, pt3 is chosen such that pt1 -> pt2 -> pt3 is CW (CCW in raw Cartesian space)"""
+    x1, y1 = pt1
+    x2, y2 = pt2
+    dx,dy = x2 - x1, y2 - y1
+    c = 1/2 #math.cos(angle) #angle = -orient * math.pi / 3.0
+    s = orient*3**0.5/2 #math.sin(angle)
+    x3 = x1 + (dx * c - dy * s)
+    y3 = y1 + (dx * s + dy * c)
+    return int(x3), int(y3) #round() is still float
